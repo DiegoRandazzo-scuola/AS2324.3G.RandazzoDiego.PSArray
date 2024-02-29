ff22dbe7917faf18afd9ddddb949d66bf7ced058
Diego Randa <diegorandazzo69@gmail.com>
2024-02-29 09:14:03 +0100

    fit: implementazione funzione MediaPonderata

--- AS2324.3G.RandazzoDiego.PSArray/Program.cs
+++ AS2324.3G.RandazzoDiego.PSArray/Program.cs
@@ -6,12 +6,18 @@
         {
             Console.WriteLine("anno scolastico 2023/2024, Randazzo Diego 3G, verifica sugli array paralleli");
             Console.WriteLine("inserisci la dimensione dell'array");
-            int dim=Convert.ToInt32(Console.ReadLine());
-            double[] voti = new double[dim];
-            int[] peso = new int[dim];
-            CaricaVettori(ref voti, ref peso);
-            StampaVotiPesi(ref voti, ref peso);
-            StampaVotiDispariMaggioriDi4(ref voti, ref peso);
+            int nVoti=Convert.ToInt32(Console.ReadLine());
+            double[] voti = new double[nVoti];
+            int[] pesi = new int[nVoti];
+            double max = 0, min = 11;
+            int posmax=0, posmin=0;
+            CaricaVettori(ref voti, ref pesi);
+            StampaVotiPesi(ref voti, ref pesi);
+            StampaVotiDispariMaggioriDi4(ref voti, ref pesi);
+            MediaPonderata(voti, pesi, ref max, ref posmax, ref min, ref posmin);
+            double mediaP = MediaPonderata; 
+            Console.WriteLine($"la media ponderata è {mediaP}");
+            Console.WriteLine($"il valore minimo è {min} in posizione {posmin} e il valore più grande è {max} in posizione {posmax}");
         }
         static void CaricaVettori(ref double[] voti, ref int[] peso)
         {
@@ -22,23 +28,54 @@
                 peso[i] = random.Next(0, 101);
             }
         }
-        static void StampaVotiPesi(ref double[] voti, ref int[] peso)
+        static void StampaVotiPesi(ref double[] voti, ref int[] pesi)
         {
             for (int i = 0;i < voti.Length; i++)
             {
                 Console.WriteLine($"il voto {i + 1} è {voti[i]}" );
-                Console.WriteLine($"il peso {i + 1} è {peso[i]}");
+                Console.WriteLine($"il peso {i + 1} è {pesi[i]}");
             }
         }
-        static void StampaVotiDispariMaggioriDi4(ref double[] voti, ref int[] peso)
+        static void StampaVotiDispariMaggioriDi4(ref double[] voti, ref int[] pesi)
         {
-            for(int i = 0; i<voti.Length; i+=2)
+            
+            for(int i = 0; i<voti.Length; i++)
             {
-                if(voti[i] > 4)
+                if (i % 2 == 0)
                 {
-                    StampaVotiPesi(ref voti, ref peso);
+                   if(voti[i] > 4)
+                   {
+                      StampaVotiPesi(ref voti, ref pesi);
+                   }
+
                 }
+                
             }
+        }
+        static double MediaPonderata(double[] voti, int[] pesi, ref double max, ref int posmax, ref double min, ref int posmin)
+        {
+            double num=0;
+            double somma = 0;
+            int conta = 1;
+            for(int i=0; i<voti.Length; i++)
+            {
+                num += voti[i] * pesi[i];
+                somma += pesi[i];
+                if (voti[i] > max)
+                {
+                    max= voti[i];
+                    posmax = conta;
+                }
+                if (voti[i] < min)
+                {
+                    min= voti[i];
+                    posmin = conta;
+                }
+                conta++;
+            }
+            double mediaP = num / somma;
+            return mediaP;
+            
         }
     }
 }
