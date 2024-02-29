b72c308b4d73436daa3d7f47056a1cb35aeceda5
Diego Randa <diegorandazzo69@gmail.com>
2024-02-29 08:35:08 +0100

    fit: implementazione della funzione caricavettori

--- AS2324.3G.RandazzoDiego.PSArray/Program.cs
+++ AS2324.3G.RandazzoDiego.PSArray/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("anno scolastico 2023/2024, Randazzo Diego 3G, verifica sugli array paralleli");
+            Console.WriteLine("inserisci la dimensione dell'array");
+            int dim=Convert.ToInt32(Console.ReadLine());
+            double[] voti = new double[dim];
+            int[] peso = new int[dim];
         }
+        static void CaricaVettori(ref double[] voti, ref int[] peso)
+        {
+            Random random = new Random();
+            for (int i = 0; i < voti.Length; i++)
+            {
+                voti[i] = random.Next(1, 11);
+                peso[i] = random.Next(0, 101);
+            }
+        }
+        
     }
 }
